43388e0f920a690102c35ef4d04d768b547512f3
Yurii Parkhomenko <parkhomenko.yurij@gmail.com>
2022-05-22 10:45:50 +0300

    add new project

---
+++ src/main/java/Main.java
@@ -0,0 +1,42 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.*;
+
+import java.util.List;
+
+public class Main {
+    private static final String NASA_URL = "https://api.nasa.gov/planetary/apod?api_key=FRl1zW5Em5OOjWU7bWEx23HYqpz1tL4dHhXA64O0";
+
+    public static final ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws IOException {
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();
+
+        HttpGet request = new HttpGet(NASA_URL);
+
+        CloseableHttpResponse response = httpClient.execute(request);
+        Nasa nasa = mapper.readValue(response.getEntity().getContent(), Nasa.class);
+
+        HttpGet request2 = new HttpGet(nasa.getHdurl());
+        CloseableHttpResponse response2 = httpClient.execute(request2);
+        byte[] bytes = response2.getEntity().getContent().readAllBytes();
+        File file = new File("nasa_imafe.jpg");
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
+        bos.write(bytes);
+        bos.flush();
+        bos.close();
+    }
+}
